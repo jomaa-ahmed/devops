5b456c9de839de43cf4242e5f5e205a66bff426f
Ahmed <ahmed.jomaa@esprit.tn>
2024-07-04 11:26:10 +0100

    change instance type to Ubuntu

--- tf2/ec2.tf
+++ tf2/ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-  ami             = "ami-06c68f701d8090592"
+  ami             = "ami-053b0d53c279acc90"
   instance_type   = "t2.micro"
   key_name        = "ec2"
   //security_groups = ["demo-sg"]
